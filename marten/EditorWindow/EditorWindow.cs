07da46b0bb822363f9e6719e3be0776756066419
martenide <vovanchig@yandex.ru>
2014-12-18 23:37:29 +0300

    small fix

---
+++ marten/EditorWindow/EditorWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+
+namespace marten
+{
+    public partial class EditorWindow : DockContent
+    {
+        /// <summary>
+        /// Filepath opened in editor
+        /// </summary>
+        public string filepath;
+
+        public EditorWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public bool OpenFile()
+        {
+            if (!File.Exists(filepath))
+                return false;
+            codeEditor.OpenFile(filepath);
+            return true;
+        }
+
+        /// <summary>
+        /// Used for save docking layout with some settings.
+        /// </summary>
+        /// <returns></returns>
+        protected override string GetPersistString()
+        {
+            // ToDo: maybe replace separateFile with settings?
+            // Add extra information into the persist string for this document
+            // so that it is available when deserialized docking solution layout.
+            return String.Concat(GetType().ToString(), App.PersistentStringSeparator, filepath);
+        }
+
+    }
+}
---
+++ marten/SolutionWindow/FileTree.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using FastTreeNS;
+
+namespace marten
+{
+    public class FileTree : FastTreeNS.FastTree
+    {
+        protected Int32 minNeededWidth;
+        protected Int32 neededWidth;
+        
+        public new event EventHandler<PaintNodeContentEventArgs> PaintNode;
+        public new event EventHandler<NodeExpandedStateChangedEventArgs> NodeExpandedStateChanged;
+
+        public FileTree() : base()
+        {
+            minNeededWidth = Int32.MaxValue;
+            SelectionColor = Color.FromArgb(51, 153, 255);
+        }
+
+
+        protected override void DrawItem(Graphics gr, VisibleItemInfo info)
+        {
+            #region Horizontal Autoscroll 
+            // expand width if needed
+            neededWidth = info.X_Text + (Int32)gr.MeasureString(info.Text, Font).Width + Padding.Right;
+            if (minNeededWidth > neededWidth)
+                minNeededWidth = neededWidth;
+            if (Width < neededWidth)
+                Width = neededWidth;
+            #endregion
+            if (PaintNode != null)
+                PaintNode(this, new PaintNodeContentEventArgs { Graphics = gr, Info = info, Node = nodes[info.ItemIndex] });
+            else
+                base.DrawItemWhole(gr, info);
+        }
+
+        protected override void OnItemCollapsed(int itemIndex)
+        {
+            #region Horizontal Autoscroll
+            // autoset minWidth
+            if (minNeededWidth < Width)
+                Width = minNeededWidth;
+            #endregion
+
+            if (NodeExpandedStateChanged != null)
+                NodeExpandedStateChanged(this, new NodeExpandedStateChangedEventArgs { Node = nodes[itemIndex], Expanded = false });
+
+            base.OnItemCollapsed(itemIndex);
+        }
+
+        public override void DrawSelection(Graphics gr, VisibleItemInfo info)
+        {
+            var rect = info.TextAndIconRect;
+            if (FullItemSelect)
+            {
+                var cr = ClientRectMinusPaddings;
+                rect = new Rectangle(cr.Left, rect.Top, cr.Width - 1, rect.Height);
+            }
+            if (rect.Width <= 0 || rect.Height <= 0) 
+                return;
+            using(var brush = new SolidBrush(SelectionColor))
+            using (var pen = new Pen(SelectionColor))
+            {
+                gr.FillRectangle(brush, Rectangle.FromLTRB(rect.Left, rect.Top, rect.Right + 1, rect.Bottom + 1));
+                gr.DrawRectangle(pen, rect);
+            }
+        }
+
+    }
+}
